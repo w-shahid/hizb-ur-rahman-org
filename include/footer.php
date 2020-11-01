77cd626a04cc84454f0f1297151eceaa39066e11
w-shahid <vikrocx@yahoo.com>
2020-11-01 07:24:46 -0700

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,75 @@
+<!-- BIG FOOTER START -->
+<div class="homepage_divider"></div>
+	
+	<section class="homepage_widgets_bg clearfix">
+		<div class="container clearfix">
+		
+			<div class="padding20"></div>
+			
+			<!-- START COL 1/2 -->
+			<div class="col_1_2">
+				<h1 class="regular red bottom_line"><strong>Multimedia Galleries</strong></h1>
+				<div class="col_1_4">
+					<img src="../images/home_image_1.png" class="liquid" alt="" />
+					
+					<h4 class="regular red"><strong><a href="../Video_Channel/video_channel.php">Video Gallery</strong></a></h4>
+					<p>Special exclusive videos  </p> 
+					
+				</div>
+				<div class="col_1_4 last">
+					<img src="../images/home_image_2.png" class="liquid" alt="" />
+					
+					<h4 class="regular red"><strong><a href="../pictures.php">Photo Gallery</strong></h4></a>
+					<p>All latest photos and event coverages available  </p> 
+				</div>
+				
+				<div class="clear padding10"></div>
+	    <!-- END COL 1/2 --></div>
+	</section>
+	<!-- BIG FOOTER END -->
+
+
+
+
+	<!-- START FOOTER SMALL -->
+	<footer class="footer_bg_bottom clearfix">
+		<div class="footer_bottom container">
+			<div class="col_2_3">
+				<!-- START FOOTER MENU -->
+				<div class="menu">
+					<ul>
+						<li><a href="../index.php">Home</a></li>
+						<li><a href="../pictures.php">Hizb ur Rahman</a></li>
+						<li><a href="#">Islamic Academy</a></li>
+						<li><a href="../Video_Channel/video_channel.php">Multimedia</a></li>
+						<li><a href="../contact.php">Contact</a></li>
+					</ul>
+				</div>
+				<!-- END FOOTER MENU -->
+				
+				<div class="clear padding20"></div>
+				
+				<!-- START FOOTER LINKS -->
+				<p>
+					<a href="#">All rights reserved</a>  &nbsp;&nbsp;|&nbsp;&nbsp;  <a href="#">Privacy Policy</a>  &nbsp;&nbsp;|&nbsp;&nbsp;  <a href="#">Terms and Conditions</a>
+				</p>
+				<!-- END FOOTER LINKS -->
+			</div>
+			<div class="col_1_3 last">
+			<!-- START KEEP IN TOUCH -->
+<!--				<div class="keep_in">Keep in touch</div>
+				<ul class="social_footer">
+					<li><a href="#"><span style="background-image:url('images/footer/twitter.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/facebook.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/skype.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/linkedin.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/vimeo.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/yahoo.png');"></span></a></li>
+					<li><a href="#"><span style="background-image:url('images/footer/flickr.png');"></span></a></li>
+				</ul>
+			</div>-->
+			
+			<div class="clear padding20"></div>
+		</div>
+	</footer>
+	<!-- END FOOTER SMALL -->
---
+++ include/header.php
@@ -0,0 +1,82 @@
+	<!-- START HEADER -->
+	<header class="header_bg clearfix">
+		<div class="container clearfix">
+			 <!-- START LOGO -->
+			<div class="logo">
+				<a href="../index.php"><img src="/images/logo.png" alt="" /></a>
+			</div>
+			 <!-- END LOGO -->
+			
+			<!-- START NAVIGATION -->
+			<nav class="main-menu">
+				<ul>
+					<li><a href="../index.php">Home</a></li>
+					<li>
+						<a>Hizb-ur-Rahman</a>
+						    <ul>
+                                <!-- commented for time being
+							<li><a href="#">Introduction</a></li>
+							<li><a href="#">Foundation</a></li>
+							    -->
+							<li><a href="../aghraz.php">Aims & Objectives</a></li>
+                                <!-- commented for time being
+							<li><a href="#">Constitution</a></li>
+							<li><a href="#">Board of Directors</a></li>
+                                -->
+
+                            <li>
+                                <a><b>Islamic Academy</b></a>
+                                <ul>
+                                    <li><a href="#">Foundation</a></li>
+                                    <li><a href="../principle-message.php">Message from Principle</a></li>
+                                    <li><a href="../result_matric_2015.php">Academic Performance</a></li>
+                                    <li><a href="#">Faculties and Institutes</a></li>
+                                    <li><a href="../result_admission.php">Admissions</a></li>
+                                    <!-- commented for time being
+                                    <li><a href="#">Time Table</a></li>
+                                    <li><a href="#">Facilities</a></li>
+                                    -->
+                                </ul>
+                            </li>
+
+							<li><a>Shaykh-e-Kamil</a>
+								<ul>
+									<li><a href="#">Biography</a></li>
+									<li><a href="#">Shajrah</a></li>
+									<li><a href="#">Shrine</a></li>
+                                    <li><a href="#">Urs sharif Mehfils</a></li>
+                                    <li><a href="#">Sayings</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+
+
+					<li>
+                        <a href="../upload_content.php"><b style="background-color:powderblue">Upload Content</b></a>
+					</li>
+
+                    <li>
+                        <a href="../Video_Channel/video_channel.php"><b>Video Channel</b></a>
+                    </li>
+
+					<li><a>Multimedia</a>
+                    	<ul>
+							<li><a href="../fikr-e-nawaz_view.php">Books and Publications</a></li>
+							<li><a href="../pictures.php">Photo Gallery</a></li>
+                            <li><a href="../Video_Channel/video_channel.php"><b>Video Channel</b></a></li>
+							<li><a href="../Video_Channel/video_channel.php">Speeches of Hazrat Syed Faraz Shah</a></li>
+                            <li><a href="#">Upcoming Events</a></li>
+                            <li><a href="#">News</a></li>
+						</ul>
+                    </li>
+                    <li>
+                        <a href="../contact.php"><b>Ask Questions</b></a>
+                    </li>
+
+				</ul>
+			</nav>
+			<!-- END NAVIGATION -->
+		</div>
+	</header>
+	<!-- END HEADER -->
