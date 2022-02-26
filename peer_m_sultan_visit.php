0c2e6bb25e1f30df75e6a50f5063bb665c32b8b1
w-shahid <vikrocx@yahoo.com>
2022-02-26 14:28:57 +0100

    adjust height of responsiveness

--- peer_m_sultan_visit.php
+++ peer_m_sultan_visit.php
@@ -63,7 +63,7 @@
                     <iframe width="560" height="480" src="https://www.youtube.com/embed/5Uv4MwDqiuc"
                             title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen style="border-style: none;width: 100%; height: 480px;">
+                            allowfullscreen style="border-style: none;width: 100%; height: 430px;">
                     </iframe>
                 </div>
             </div>
