b5c90796dbb4c2928d4ce8603b67b68b26de9ddc
w-shahid <vikrocx@yahoo.com>
2022-02-26 12:35:51 +0100

    Peer Sultan visit to acadmey

---
+++ peer_m_sultan_visit.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<title>Dr. Hazrat Allama Peer Muhammad Sultan Darbar Shareef Nairian Shareef Visit to Hizb-ur-Rahman Acadmey</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="robots" content="index,follow" />
+<link rel="shortcut icon" href="images/favicon.ico" /> 
+<link rel="stylesheet" href="../fonts.googleapis.com/css@family=Lato_3A400,700" type="text/css" />
+<link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" />
+<link rel="stylesheet" href="css/stratosfera.css" type="text/css" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="../https@html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<script type="text/javascript" src="js/selectivizr-min.js"></script>
+<link rel="stylesheet" href="css/ie_7.css" type="text/css" />
+<![endif]-->
+<script src="../ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/slides.min.jquery.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="js/slides.min.jquery.js"></script>
+<script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
+<script type="text/javascript" src="js/jquery.backgroundPosition.js"></script>
+<script type="text/javascript" src="js/respond.min.js"></script>
+<script type="text/javascript" src="js/misc.js"></script>
+<script type="text/javascript" src="js/jquery.quicksand.js"></script>
+<script type="text/javascript" src="js/quick.js"></script>
+</head> 
+<body>
+	<!-- START HEADER -->
+	<?php include_once("include/header.php"); ?>
+	<!-- END HEADER -->
+	
+	<!-- START CONTENT -->
+	<section class="container clearfix">
+		<!-- START PAGE INFO -->
+			<header class="container page_info clearfix">
+				<div class="text">
+					<h1>Images and Pictures</h1>
+				</div>
+				<div class="clear"></div>
+			</header>
+			<div class="delimiter"></div>
+		<!-- END PAGE INFO -->
+		
+		<div class="col_1_4">
+			<h1 class="red">Dr. Hazrat Allama Peer Muhammad Sultan Darbar Shareef Nairian Shareef Visit to Hizb-ur-Rahman Acadmey</h1>
+		</div>
+		
+		<div class="clear padding20"></div>
+
+
+        <div class="clearfix">
+            <div class="container">
+                <div class="recent_works_left">
+                    <h3 class="blue">Dr. Hazrat Allama Peer Muhammad Sultan Visit to Hizb-ur-Rahman Acadmey</h3>
+                </div>
+            </div>
+            <div class="container">
+                <div style="background-color:#000;" class="col-md-9">
+                    <video id="working" width="100%" height="393" poster="images/Peer_M_Sultan_Visit/visit_main.png" controls preload="none">
+                        <source src="images/Peer_M_Sultan_Visit/nairian_darbar_shareef_kashmir_visit.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </div>
+        </div>
+
+		<div class="portfolio_1_3">
+			<ul id="list">
+				<!-- START PORTFOLIO COLUMN #1 -->
+				<li id="id-1" class="app">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/1.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/1.jpeg" alt="" /></a>
+					</span>
+				</li>
+
+				<li id="id-2" class="util">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/2.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/2.jpeg" alt="" /></a>
+					</span>
+					
+				</li>
+
+				<li id="id-3" class="app">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/3.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/3.jpeg" alt="" /></a>
+					</span>
+				</li>
+
+				<li id="id-4" class="util">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/4.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/4.jpeg" alt="" /></a>
+					</span>
+					<span class="title"></span>
+					
+					
+				</li>
+
+				<li id="id-5" class="app">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/5.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/5.jpeg" alt="" /></a>
+					</span>
+					<span class="title"></span>
+					
+				</li>
+
+				<li id="id-6" class="util">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/6.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/6.jpeg" alt="" /></a>
+					</span>
+					<span class="title"></span>
+					
+					
+				</li>
+
+				<li id="id-7" class="util">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/7.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/7.jpeg" alt="" /></a>
+					</span>
+					<span class="title"></span>
+					
+					
+				</li>
+
+				<li id="id-8" class="app">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/8.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/8.jpeg" alt="" /></a>
+					</span>
+                    <span class="title"></span>
+
+                </li>
+
+                <li id="id-9" class="app">
+					<span class="image">
+						<a href="images/Peer_M_Sultan_Visit/9.jpeg" data-rel="prettyPhoto" class="magnific-glass"><img class="portfolio_image" src="images/Peer_M_Sultan_Visit/9.jpeg" alt="" /></a>
+					</span>
+                    <span class="title"></span>
+
+                </li>
+				
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</section>
+	<!-- END CONTENT -->
+
+	<!-- START FOOTER SMALL -->
+<?php include_once("include/footer.php"); ?>
+	<!-- END FOOTER SMALL -->
+</body>
+</html>
